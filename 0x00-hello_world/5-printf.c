837515383dd6a078bc099998c2efa6c7e96baba7
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 11:07:54 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - begining or entry of the code body
+ * return - should return 0 if any error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

118b89a9b54fc9d83a73ef32c41311a1538ef195
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 10:52:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
  * Task 4 for puts
- * should return 0
+ * Main - beginning point of the code
+ * Return: should return 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

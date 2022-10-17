bd22eb95e1bce11e23d97fdaa4992eb95fe49a4a
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 23:30:47 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		putchar(numbers);
+		if (numbers < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

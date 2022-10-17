9a0a130a29df029fa917e06be9b1f9d9c1c024df
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 22:58:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+int main(void)
+{
+	char numberHex;
+	char alphaHex;
+
+	for (numberHex = '0'; numberhex <= '9'; numberHex++)
+		putchar (numberHex);
+	for (alphaHex = 'a'; alphaHex <= 'f'; alphaHex++)
+		putchar(alphaHex);
+		putchar('\n');
+		return (0);
+}

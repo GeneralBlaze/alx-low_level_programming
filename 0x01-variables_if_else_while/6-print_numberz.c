91123aab6c88fc92b2bc11275c88bc9a264a20e1
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 22:21:25 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main-code entry point or code begining
+ *Return:show 0 if error
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+		putchar('\n');
+		return (0);
+}

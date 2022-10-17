e5663b3e02a65c35b5198685c405265d354aa413
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 23:01:40 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 	char numberHex;
 	char alphaHex;
 
-	for (numberHex = '0'; numberhex <= '9'; numberHex++)
+	for (numberHex = '0'; numberHex <= '9'; numberHex++)
 		putchar (numberHex);
 	for (alphaHex = 'a'; alphaHex <= 'f'; alphaHex++)
 		putchar(alphaHex);

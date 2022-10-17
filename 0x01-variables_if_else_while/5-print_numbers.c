67d70669b49004e096d48882afbd956bf2ae8c2c
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-16 22:04:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main-code entry point or code begining
+ *Return:show 0 if error
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = 0 ; number < 10 ; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}

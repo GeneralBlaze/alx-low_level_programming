9b7d9547dd92501bc52c85167b69ae670c2f8a99
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-18 14:03:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the code
+ * Return: this code will retuin 0 in case of an error
+ */
+
+int main(void)
+{
+
+	putchar("_putchar\n");
+	return (0);
+
+}

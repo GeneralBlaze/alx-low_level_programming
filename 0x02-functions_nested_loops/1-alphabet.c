eb37ba213b0ff1c604363c6d5263c30cff56845f
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-19 07:41:37 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main entry point of code
+ * Return: 0 for errors
+ */
+
+int main(void)
+{
+
+	void print_alphabet(void)
+	{
+		for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+		{
+			_putchar('alphabet');
+			_putchar('\n');
+		}
+	}
+return (0);
+}

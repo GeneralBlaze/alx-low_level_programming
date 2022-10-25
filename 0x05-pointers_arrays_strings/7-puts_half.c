fbdfbaf58030a7b4a3434d4b4b3d0cb52c70ba66
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-25 16:10:54 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to work on
+ * Return: absent
+ */
+
+void puts_half(char *str)
+{
+	int a, b, c;
+
+	while (str[a] != '\0')
+	{	
+		a++;
+	}
+	if (a % 2 == 0)
+	{	
+		c = a / 2;
+	}
+	else
+	{	
+		c = (a + 1) / 2;
+	}
+	for (b = c; b < a; b++)
+	{	
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}

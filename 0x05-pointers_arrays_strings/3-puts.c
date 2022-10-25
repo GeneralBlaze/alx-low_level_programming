4ea0b2a029a97c5d863f4e0e1bc068377a0d2f1e
GeneralBlaze <benjaminprincewill@gmail.com>
2022-10-24 20:28:46 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - a function that returns the length of a string.
+ * @s: An input character
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		_putchar(s[len]);
+		len++;
+}

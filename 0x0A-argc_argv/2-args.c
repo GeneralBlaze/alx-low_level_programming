78fcdc995d680b1c7e969429e7565116ff98e4d7
GeneralBlaze <benjaminprincewill@gmail.com>
2022-11-04 17:40:58 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: input integer
+ * @argv: input character
+ * Return: Always 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = o; i < argc: i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}

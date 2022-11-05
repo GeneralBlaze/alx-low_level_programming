8c958df9956c983469511fe3d16775e05ff3168c
GeneralBlaze <benjaminprincewill@gmail.com>
2022-11-04 18:10:32 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -9,7 +10,7 @@
  */
 
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int result;
 
@@ -19,7 +20,7 @@ int main(int argc, char *argv[] __attribute__((unused)))
 	}
 	else
 	{
-		result = atoi(argc[1]) * atoi(argc[2]);
+		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
 	}
 	return (0);
